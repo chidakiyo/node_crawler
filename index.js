1618a08f987cedcd639a4bef78f5efca1546b633
Kiyokatsu Chida <chidakiyo@gmail.com>
2018-10-01 00:12:29 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const puppeteer = require('puppeteer');
+const app = express();
+
+app.use(async (req, res) => {
+    const url = req.query.url;
+
+    if (!url) {
+        return res.send('Please provide URL as GET parameter, for example: <a href="/?url=https://example.com">?url=https://example.com</a>');
+    }
+
+    const browser = await puppeteer.launch({
+        args: ['--no-sandbox']
+    });
+
+    // ページを作成する
+    const page = await browser.newPage();
+
+    // 解像度をセットする
+    // await page.setViewport({ width: 720, height: 600 })
+
+    await page.goto(url);
+    const imageBuffer = await page.screenshot();
+    browser.close();
+
+    res.set('Content-Type', 'image/png');
+    res.send(imageBuffer);
+});
+
+const server = app.listen(process.env.PORT || 8080, err => {
+    if (err) return console.error(err);
+    const port = server.address().port;
+    console.info(`App listening on port ${port}`);
+});
